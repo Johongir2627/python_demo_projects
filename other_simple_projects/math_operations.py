acd7d69d9a6fc12477245439036545ff4cd87800
Johongir Dagarov <johongirdagarov@gmail.com>
2025-12-05 20:59:57 +0500

    Add files via upload

--- other_simple_projects/math_operations.py
+++ other_simple_projects/math_operations.py
@@ -1,1 +1,15 @@
+def add(x, y):
+    return x + y
 
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    try:
+        a = x / y
+        return a
+    except:
+        return "Invalid"
